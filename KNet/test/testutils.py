22eefa851bf031f91b408be14ba503f49a701eb0
suresh <sureshkumarr.s@gmail.com>
2018-01-12 12:00:47 +0100

    test cases fixed

--- KNet/test/testutils.py
+++ KNet/test/testutils.py
@@ -5,14 +5,14 @@ import KNet.lib.utils as utils
 class TestUtils(unittest.TestCase):
 
     def test_generate_id(self):
-        self.assertEqual(utils.generate_id(), 1)
+        self.assertEqual(utils.generate_id(), 0)
 
     def test_generate_id_again(self):
-        self.assertEqual(utils.generate_id(), 2)
+        self.assertEqual(utils.generate_id(), 1)
 
     def test_reset_generate(self):
         utils.reset_id()
-        self.assertEqual(utils.generate_id(), 1)
+        self.assertEqual(utils.generate_id(), 0)
 
 if __name__ == '__main__':
     unittest.main()
